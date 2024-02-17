46db3d2f7019117408311c9b1cce24c2b726dbf6
Hardeep6singh <hharrys.singh467@gmail.com>
2024-02-17 13:43:47 -0500

    added public IP for Jfrog integartion

--- vars/jFrogIntegration.groovy
+++ vars/jFrogIntegration.groovy
@@ -1,3 +1,3 @@
 def call(){
-    sh 'curl -X PUT -u admin:Harry@6123 -T /var/lib/jenkins/workspace/Java_app_3.0/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://18.205.27.195:8082/artifactory/example-repo-local-bash/'
+    sh 'curl -X PUT -u admin:Harry@6123 -T /var/lib/jenkins/workspace/Java_app_3.0/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://54.88.44.182:8082/artifactory/example-repo-local-bash/'
 }
